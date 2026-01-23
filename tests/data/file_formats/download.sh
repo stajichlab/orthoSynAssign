e53c9fa1174f54c9a41d529870cd6681732bb6c3
Jason Stajich <jasonstajich.phd@gmail.com>
2026-01-23 14:52:10 -0800

    add the test files

---
+++ tests/data/file_formats/download.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash -l
+
+#NCBI
+wget https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/865/725/GCF_000865725.1_ViralMultiSegProj15521/GCF_000865725.1_ViralMultiSegProj15521_genomic.gff.gz
+https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/865/725/GCF_000865725.1_ViralMultiSegProj15521/GCF_000865725.1_ViralMultiSegProj15521_genomic.gtf.gz

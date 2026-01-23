b9e8268d1c3038eafd17c342dba74dabc036820d
Jason Stajich <jasonstajich.phd@gmail.com>
2026-01-23 11:51:35 -0800

    add author info

--- orthoSynAssign/__init__.py
+++ orthoSynAssign/__init__.py
@@ -1,9 +1,9 @@
 """
 orthoSynAssign - Ortholog Synteny Assignment Tool
 
-A tool for analyzing orthologous groups and synteny using OrthoFinder results
+A tool for analyzing orthologous groups and synteny using OrthoFinder or other tool results
 and genome annotation files (GFF3/GTF).
 """
 
 __version__ = "0.1.0"
-__author__ = "Your Name"
+__author__ = "Jason Stajich"

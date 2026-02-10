3e67a2d3e1a88e74d5ff4e1a1ba6d5b68a2ea48e
Cheng-Hung Tsai <chenghung.tsai@email.ucr.edu>
2026-02-10 11:31:45 -0800

    Force to sort the converted bed files.

--- tests/data/Afum_3/convert_bed.sh
+++ tests/data/Afum_3/convert_bed.sh
@@ -1,3 +1,3 @@
 for gff in *.gff.gz; do
-    zcat $gff | awk -F "\t" -v OFS="\t" '$3=="protein_coding_gene"{split($9, a, /[=;]/); print$1,$4,$5, a[2]}' > ${gff%.gff.gz}.bed
+    zcat $gff | awk -F "\t" -v OFS="\t" '$3=="protein_coding_gene"{split($9, a, /[=;]/); print$1,$4,$5, a[2]}' | sort -k 1,1 -k2,2n > ${gff%.gff.gz}.bed
 done
